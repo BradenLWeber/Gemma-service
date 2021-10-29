8cef6f2f459f846bce5f6fe2f4366d04601438c4
yj225 <yj225@gold20.cs.calvin.edu>
2021-10-29 16:20:21 -0400

    this is a trial

---
+++ sql/gemma-queries.sql
@@ -0,0 +1,14 @@
+-- Get the number of AUser records.
+SELECT *
+  FROM AUser
+  ;
+
+-- Get the Coordinates records.
+SELECT * 
+  FROM Coordinates
+  ;
+
+-- Get Tag's from Pins put in the Coordinate records
+SELECT *
+  FROM Tag
+ ;
